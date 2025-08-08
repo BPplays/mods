22fe6aaa943a9e72a383539ba0032f99d4b78b61
BPplays <suzuko.oss@proton.me>
2025-08-07 18:28:08 -0700

    working?

--- cyberpunk2077/auto_sprint/init.lua
+++ cyberpunk2077/auto_sprint/init.lua
@@ -1,21 +1,70 @@
 
+
+SprintObj = false
+SprintSet = false
+
 Player = false
 IsSprinting = false
 
 local triedSprint = false
 ForceSprint = true
+local enableLogging = true
+local ignoreActions = {
+	['BUTTON_RELEASED'] = {
+		['UI_FakeMovement'] = true,
+	},
+	['RELATIVE_CHANGE'] = {
+		['UI_FakeCamera'] = true,
+		['CameraMouseX'] = true,
+		['CameraMouseY'] = true,
+		['mouse_x'] = true,
+		['mouse_y'] = true,
+	},
+}
+
 
 registerForEvent('onInit', function()
 	Player = Game.GetPlayer()
 	dmp = Dump(Player, false)
 	dmp = DumpType('PlayerPuppet', false)
-	spdlog.debug(dmp)
+	-- spdlog.info(dmp)
 	-- print(dmp)
 	Player:RegisterInputListener(Player)
+
+
 
 	Observe('PlayerPuppet', 'OnGameAttached', function(self)
 		self:RegisterInputListener(self)
 	end)
+
+	Observe("PlayerPuppet", "OnAction", function(_, action)
+		-- print(Game.NameToString(action:GetName()))
+		if not action then return end
+
+		spdlog.info(Dump(action))
+		-- print(Dump(action))
+
+		if action:GetType().value == "BUTTON_PRESSED" then
+			if Game.NameToString(action:GetName()) == "Sprint" then
+				-- Sprint button is pressed (or active)
+				print("Sprinting!")
+				SprintObj = action
+				SprintSet = true
+			end
+		end
+
+		if enableLogging then
+			local actionName = Game.NameToString(action:GetName())
+			local actionType = action:GetType().value -- gameinputActionType
+			local actionValue = action:GetValue()
+
+			if not ignoreActions[actionType] or not ignoreActions[actionType][actionName] then
+				spdlog.info(('[%s] %s = %.3f'):format(actionType, actionName, actionValue))
+			end
+		end
+	end)
+
+
 
     Observe('SprintEvents', 'OnEnter', function()
         IsSprinting = true
@@ -25,28 +74,27 @@ registerForEvent('onInit', function()
     end)
 
 
-		act = NewObject("ListenerAction")
-		print(dump(act))
 
 	Override('SprintDecisions', 'OnAction', function(self, action, consumer, wrapped)
-		-- Optionally call original logic
 
-		-- If you want to force sprint behavior:
-		-- You can't set m_sprintPressed directly (private), but you could call
-		-- EnableOnEnterCondition(true) or replicate the logic.
-
-		-- Example: always enable sprint condition when a custom flag is set
 		if (ForceSprint and (not IsSprinting)) then
 			self:EnableOnEnterCondition(true)
-			-- maybe prevent original logic continuation
 			-- return true
 		end
 
-		act = NewObject("ListenerAction")
-		print(dump(action))
+		-- act = NewObject("ListenerAction")
+		-- act.Name = "sprint"
+
+
+		if SprintSet then
+			action = SprintObj
+		end
+
+		spdlog.info(Dump(action))
+		print(Dump(action))
 
 		-- local res = wrapped(action, consumer)
-		local res = wrapped("sprint", consumer)
+		local res = wrapped(action, consumer)
 		return res
 	end)
 end)
