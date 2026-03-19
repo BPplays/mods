b9dcf3d88997b841f2420e3deeebcdd13bd9c1db
BPplays <suzuko.oss@proton.me>
2026-03-18 21:05:01 -0700

    test

---
+++ the_long_drive/stutter_fix/Plugin.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace stutter_fix;
+
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("net.BPplays.interpolationfix", "Interpolation Fix", "1.0.0")]
+public class InterpolationFixPlugin : BaseUnityPlugin
+{
+    private Harmony _harmony;
+
+    private void Awake()
+    {
+        _harmony = new Harmony("net.BPplays.interpolationfix");
+        _harmony.PatchAll();
+        Logger.LogInfo("Interpolation Fix loaded");
+    }
+
+    private void OnDestroy()
+    {
+        _harmony?.UnpatchSelf();
+    }
+
+    public static void ApplyInterpolation(Object obj)
+    {
+        if (obj == null)
+            return;
+
+        if (obj is GameObject go)
+        {
+            ApplyToGameObject(go);
+            return;
+        }
+
+        if (obj is Component component)
+        {
+            ApplyToGameObject(component.gameObject);
+        }
+    }
+
+    private static void ApplyToGameObject(GameObject go)
+    {
+        var rigidbodies = go.GetComponentsInChildren<Rigidbody>(true);
+        foreach (var rb in rigidbodies)
+        {
+            if (!rb.isKinematic)
+                rb.interpolation = RigidbodyInterpolation.Interpolate;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(Object), nameof(Object.Instantiate), new[] { typeof(Object) })]
+internal static class InstantiatePatch
+{
+    private static void Postfix(Object __result)
+    {
+        InterpolationFixPlugin.ApplyInterpolation(__result);
+    }
+}
---
+++ the_long_drive/stutter_fix/obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace stutter_fix
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "stutter_fix";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
